fee7b70ca287c3f92e60db6ae6d1e923f6f9733d
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-05-12 23:16:28 +0100

    Added Search by star

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# load chrome driver
 PATH = "/Users/s2124052/Downloads/chromedriver"
 driver = webdriver.Chrome(PATH)
 
+# load webpage
 driver.get("http://promoters.wormguides.org/search.php")
-print(driver.title)
+search = driver.find_element_by_name("q")
+search.send_keys("*")
+search.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
 class Scrapper:
 
     def __init__ (self):
