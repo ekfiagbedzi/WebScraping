cf2a835b858633a9bc8e2a097bbf40dc7bf5e639
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-05-12 23:57:54 +0100

    Click on Resources

--- web_scraping.py
+++ web_scraping.py
@@ -7,10 +7,23 @@ PATH = "/Users/s2124052/Downloads/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 # load webpage
-driver.get("http://promoters.wormguides.org/search.php")
-search = driver.find_element_by_name("q")
-search.send_keys("*")
-search.send_keys(Keys.RETURN)
+driver.get("https://wormguides.org/")
+resources_tab = driver.find_element_by_xpath('//a[@href="'+"https://wormguides.org/resources/"+'"]')
+resources_tab.click()
+
+time.sleep(10)
+
+neuron_specific_marker_genes = driver.find_element_by_xpath('//a[@href="'+"https://wormguides.org/neuron-specific-marker-genes/"+'"]')
+time.sleep()
+neuron_specific_marker_genes.click()
+
+time.sleep(10)
+
+
+#search = driver.find_element_by_name("q")
+#search.clear()
+#search.send_keys("*")
+#search.send_keys(Keys.RETURN)
 
 time.sleep(10)
 
