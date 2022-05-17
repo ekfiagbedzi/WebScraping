84690ad2192194f9551a4ea05d20b532743cd7ce
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-05-17 19:11:18 +0100

    Created list of Expression Details links

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,11 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
 
 # load chrome driver
 PATH = "/Users/s2124052/Downloads/chromedriver"
@@ -11,36 +13,41 @@ driver = webdriver.Chrome(PATH)
 
 # load webpage
 driver.get("https://wormguides.org/")
-resources_tab = driver.find_element_by_xpath('//a[@href="'+"https://wormguides.org/resources/"+'"]')
+resources_tab = driver.find_element(
+    by=By.XPATH, value='//a[@href="'+"https://wormguides.org/resources/"+'"]')
 resources_tab.click()
 
 time.sleep(10)
 
-neuron_specific_marker_genes = driver.find_element_by_xpath('//a[@title="Neuron-Specific Marker Genes"]')
+neuron_specific_marker_genes = driver.find_element(
+    by=By.XPATH, value='//a[@title="Neuron-Specific Marker Genes"]')
 
 time.sleep(10)
 neuron_specific_marker_genes.click()
 time.sleep(10)
 
 
-promoters = driver.find_element_by_xpath('//a[@href="'+"http://promoters.wormguides.org/"+'"]')
+promoters = driver.find_element(
+    by=By.XPATH, value='//a[@href="'+"http://promoters.wormguides.org/"+'"]')
 promoters.click()
 
 time.sleep(5)
 
-search = driver.find_element_by_name("q")
+search = driver.find_element(by=By.NAME, value="q")
 search.clear()
 search.send_keys("*")
 search.send_keys(Keys.RETURN)
 
 try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "results_container")))
-    classes = driver.find_elements(by=By.CLASS_NAME, value="result_container")
-    for cl in classes:
-        ccl = cl.find_element(by=By.TAG_NAME, value="a")
-    print(ccl.get_attribute("href"))
-finally:
+    elements = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "result_container")))
+    expression_detail_links = []
+    for element in elements:
+        expression_detail = element.find_element(by=By.TAG_NAME, value="span")
+        expression_detail_link = expression_detail.get_attribute("href")
+        expression_detail_links.append(expression_detail_link)
+
+except:
     driver.quit()
 
 class Scrapper:
@@ -58,3 +65,6 @@ class Scrapper:
 
     def search():
         pass
+
+
+
