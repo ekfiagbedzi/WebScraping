ed040058a4e59e4969db9980598d845431fe24bd
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-06-15 23:07:14 +0100

    Added unittests for load_webpage, switch_driver and click

---
+++ test.py
@@ -0,0 +1,33 @@
+from web_scraping import Scrapper
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class ScrapperTestCase(unittest.TestCase):
+    
+    def test_load_webpage(self): # load correct webpage
+        scrapper = Scrapper("https://wormguides.org/")
+        expected_value = None
+        actual_value = scrapper.load_webpage()
+        self.assertEqual(expected_value, actual_value)
+
+
+    def test_switch_driver(self): # driver has been switched
+        scrapper = Scrapper("https://wormguides.org/")
+        expected_value = None
+        actual_value = scrapper.switch_driver("/home/biopythoncodepc/Documents/chromedriver")
+        self.assertEqual(expected_value, actual_value)
+
+    
+    def test_click(self): # click a button
+        scrapper = Scrapper("https://wormguides.org/")
+        expected_value = None
+        actual_value = scrapper.click(By.XPATH, "https://wormguides.org/resources/", "href")
+        self.assertEqual(expected_value, actual_value)
+        
+
+unittest.main(argv=[''], verbosity=1, exit=False)
