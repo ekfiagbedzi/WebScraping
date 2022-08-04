76b742a3169d8938077d0b2196db290f1ced0f43
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-08-04 21:47:32 +0100

    reorganise files

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from web_scraping import Scrapper
+from utils.worm_scrapper import WormScrapper
 import unittest
 
 from selenium import webdriver
@@ -10,21 +10,21 @@ from selenium.webdriver.support import expected_conditions as EC
 class ScrapperTestCase(unittest.TestCase):
     
     def test_load_webpage(self): # load correct webpage
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = None
         actual_value = scrapper.load_webpage()
         self.assertEqual(expected_value, actual_value)
 
 
     def test_switch_driver(self): # driver has been switched
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = None
         actual_value = scrapper.switch_driver("/home/biopythoncodepc/Documents/chromedriver")
         self.assertEqual(expected_value, actual_value)
 
     
     def test_click(self): # click a different button
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = None
         scrapper.load_webpage()
         actual_value = scrapper.click(By.XPATH, "https://wormguides.org/technologies/", "href")
@@ -32,7 +32,7 @@ class ScrapperTestCase(unittest.TestCase):
         
 
     def test_search(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = None
         scrapper.load_webpage()
         scrapper.click(By.XPATH, "https://wormguides.org/resources/", "href")
@@ -44,7 +44,7 @@ class ScrapperTestCase(unittest.TestCase):
 
 
     def test_forward(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = None
         scrapper.load_webpage()
         scrapper.click(By.XPATH, "https://wormguides.org/technologies/", "href")
@@ -53,7 +53,7 @@ class ScrapperTestCase(unittest.TestCase):
 
 
     def test_back(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = None
         scrapper.load_webpage()
         scrapper.click(By.XPATH, "https://wormguides.org/technologies/", "href")
@@ -61,7 +61,7 @@ class ScrapperTestCase(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
 
     def test_get_element_attribute(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = str
         scrapper.load_webpage()
         element = scrapper.find_element(By.XPATH, "https://wormguides.org/wormguides-data/", "href")
@@ -69,7 +69,7 @@ class ScrapperTestCase(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
     
     def test_get_element_attribute_from_list(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = list
         scrapper.load_webpage()
         element = scrapper.find_elements(By.TAG_NAME, "script")
@@ -77,13 +77,13 @@ class ScrapperTestCase(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
 
     def test_generate_uuids(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = 5
         actual_value = len(scrapper.generate_uuids(5))
         self.assertEqual(expected_value, actual_value)
 
     def test_download_image(self):
-        scrapper = Scrapper("https://wormguides.org/")
+        scrapper = WormScrapper("https://wormguides.org/")
         expected_value = "/home/biopythoncodepc/Downloads/0d2fb674-4b6d-420b-a0e7-38c09869e5dd.gif"
         actual_value = scrapper.download_image("http://promoters.wormguides.org/showImage.php?image_id=128", "/home/biopythoncodepc/Downloads/", "0d2fb674-4b6d-420b-a0e7-38c09869e5dd")
         self.assertEqual(expected_value, actual_value)    
